8fbc120c8dae52038699cc994bf80c479caab790
j29382165 <j2938216501@gmail.com>
2025-01-12 10:22:55 +0800

    帳號功能-使用MD5加密密碼

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -10,7 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class UserServiceImpl implements UserService { //加Component,讓UserServiceImpl成為Bean
@@ -30,6 +33,10 @@ public class UserServiceImpl implements UserService { //加Component,讓UserServ
             //log.warn("該email{}已被{}註冊",userRegisterRequest.getEmail(),"Linda");//{}大括號放參數
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST); //BAD_REQUEST 400
         }
+
+        //使用MD5加密密碼，生成雜湊
+        String hashedPassword= DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
         //創建帳號
         return userDao.createUser(userRegisterRequest);
 
@@ -48,7 +55,10 @@ public class UserServiceImpl implements UserService { //加Component,讓UserServ
             log.warn("該email:{}尚未被註冊",userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST); //強制停止前端請求400 BAD_REQUEST
         }
-        else if(user.getPassword().equals(userLoginRequest.getPassword())){//String比較要用equals,非==
+
+        String hashedPassword= DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        if(user.getPassword().equals(hashedPassword)){ //String比較要用equals,非==
             return user;
         }
         else{
@@ -56,5 +66,8 @@ public class UserServiceImpl implements UserService { //加Component,讓UserServ
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST); //強制停止前端請求400 BAD_REQUEST
         }
     }
+
+
+
 
 }
