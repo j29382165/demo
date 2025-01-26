20f8c4c4aa39d1e812d5951ebeafe9c5f6ae53c7
j29382165 <j2938216501@gmail.com>
2025-01-26 14:23:37 +0800

    單元測試：訂單功能-查詢訂單列表

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -22,5 +22,23 @@ CREATE TABLE IF NOT EXISTS user
     last_modified_date TIMESTAMP    NOT NULL
     );
 
+-- 一定要加IF NOT EXISTS,避免重複創建table order,訂單測試 --
+CREATE TABLE IF NOT EXISTS `order`
+(
+    order_id           INT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id            INT       NOT NULL,
+    total_amount       INT       NOT NULL,
+    created_date       TIMESTAMP NOT NULL,
+    last_modified_date TIMESTAMP NOT NULL
+);
 
+-- 一定要加IF NOT EXISTS,避免重複創建table order_item
+CREATE TABLE IF NOT EXISTS order_item
+(
+    order_item_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    order_id      INT NOT NULL,
+    product_id    INT NOT NULL,
+    quantity      INT NOT NULL,
+    amount        INT NOT NULL
+);
 
